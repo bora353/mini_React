3e45b8ccb5970a048ce7f9401c6c7b2374e6b8c0
이가은 <gaeun.lee@mirero.co.kr>
2023-10-20 17:07:05 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("/parsing")
+      .then((response) => {
+        console.log(response.data);
+        //console.log(response.data.dataMapList[0].DEFECTID);
+        //console.log(response.data.dataMapList[0]);
+        //console.log(response.data.waferID);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>React 화면~</h2>
+      {<p>lotID : {data.lotID}</p>}
+      {<p>deviceID : {data.deviceID}</p>}
+      {<p>stepID : {data.stepID}</p>}
+      {<p>equipID : {data.equipID}</p>}
+      {<p>ppID : {data.ppID}</p>}
+      {<p>resultTimestamp : {data.resultTimestamp}</p>}
+      {<p>slotNo : {data.slotNo}</p>}
+      {<p>waferID : {data.waferID}</p>}
     </div>
   );
 }
