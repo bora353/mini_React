b1826156648d5b035f5663a6578310d62f026a58
이가은 <gaeun.lee@mirero.co.kr>
2023-10-27 17:49:12 +0900

    post 하는 중

--- src/pages/SummaryPage.js
+++ src/pages/SummaryPage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Bar from "../components/Bar";
 import Title from "../components/Title";
+import Post from "../components/Post";
 
 export default function SummaryPage() {
   return (
     <div>
-      <Bar /> <Title />
+      <Bar /> <Title /> <Post />
     </div>
   );
 }
